eec57eda17c60ec53c5d55b4c123f306b3bce81a
Millisami <millisami@gmail.com>
2013-05-05 22:47:25 +0545

    version bumped to 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "millisami@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures errbit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.4.0"
 
 depends          "mongodb"
 depends          "git"
