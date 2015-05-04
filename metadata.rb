c328c6985ebe6789e3bd6d6de7d7307b779a28c9
James Le Cuirot <james.le-cuirot@yakara.com>
2015-05-04 16:01:41 +0100

    Fix licenses

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "errbit"
 maintainer       "Sachin Sagar Rai"
 maintainer_email "millisami@gmail.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures errbit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
