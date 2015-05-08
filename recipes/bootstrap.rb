b3ebbfc611cfe418845412b1ff428575235b1e3f
James Le Cuirot <james.le-cuirot@yakara.com>
2015-05-08 16:24:49 +0100

    Implement not_if in bootstrap recipe
    Like rake db:migrate, we cannot use rbenv_script here. Unfortunately
    there appears to be no way of outputting the generated password, short
    of running chef-client with :info log level.

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -1,15 +1,39 @@
+#
+# Author:: Sachin Sagar Rai <millisami@gmail.com>
+# Cookbook Name:: errbit
+# Recipe:: setup
+#
+# Copyright (C) 2013 Millisami
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-
-Chef::Log.info "-" * 70
-Chef::Log.info "Checking to bootstrap the admin user"
+home_dir = "/home/#{node['errbit']['user']}"
+current_dir = "#{node['errbit']['deploy_to']}/current"
 
 rbenv_script 'rake db:seed' do
   code 'bundle exec rake db:seed RAILS_ENV=' + node['errbit']['config']['rails_env']
-  cwd "#{node['errbit']['deploy_to']}/current"
+  cwd current_dir
   user node['errbit']['user']
-  # not_if "bundle exec rails runner 'p User.where(admin: true).first'"
-  notifies :create, "ruby_block[remove_bootstrap]", :immediately
-  # notifies :restart, "service[unicorn_#{app['id']}]"
+
+  not_if "#{home_dir}/.rbenv/bin/rbenv exec bundle exec rails runner 'exit User.where(:admin => true).exists?'", {
+    :cwd => current_dir,
+    :user => node['errbit']['user'],
+    :group => node['errbit']['group'],
+    :environment => { 'HOME' => home_dir }
+  }
+
+  notifies :run, "ruby_block[remove_bootstrap]", :immediately
 end
 
 ruby_block "remove_bootstrap" do
